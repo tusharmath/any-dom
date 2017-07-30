c6657ba851c03108995ff33e172b14bc0a880817
Tushar Mathur <tusharmath@gmail.com>
2017-07-30 20:16:15 +0530

    feat(build): add UMD build support

--- src/dom/preact.ts
+++ src/dom/preact.ts
@@ -5,7 +5,7 @@
 import {render} from 'preact'
 import VNode = preact.VNode
 
-let dom = null
+let dom: any = null
 export const patch: PatchFunction<VNode, void> = (node: HTMLElement, vNode: VNode) => {
   dom = render(vNode, node, dom)
 }
